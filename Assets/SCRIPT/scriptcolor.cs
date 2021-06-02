da318ff7226a27654258a7d6259e081198ee87f8
Whorror-cell <esperanzarodriguezcantarero@outlook.es>
2021-06-02 17:33:41 +0200

    esfera_hecha

--- Assets/SCRIPT/scriptcolor.cs
+++ Assets/SCRIPT/scriptcolor.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cambioColor : MonoBehaviour
+public class scriptcolor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,7 @@ public class cambioColor : MonoBehaviour
         if
           (Input.GetKeyDown(KeyCode.Y)) //aqui asignamos la letra que tenemos que pulsar.
         {
-            GetComponent<Renderer>().material.color = Color.cyan; //aqui el color que nos va a mostrar.
+            GetComponent<Renderer>().material.color = Color.yellow; //aqui el color que nos va a mostrar.
         }
         if
            (Input.GetKeyDown(KeyCode.K))
@@ -27,6 +27,12 @@ public class cambioColor : MonoBehaviour
            (Input.GetKeyDown(KeyCode.L))
         {
             GetComponent<Renderer>().material.color = Color.black;
+        }
+
+        if
+          (Input.GetKeyDown(KeyCode.U)) 
+        {
+            GetComponent<Renderer>().material.color = Color.green; 
         }
         //para ver cualquiera de los colores, deberá estar en play.
         //acuerdate de guardar tanto en visual como en unity antes de darle a play.
